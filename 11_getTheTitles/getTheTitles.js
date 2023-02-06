4728bde8a75599a71de0a6f7c01b840b76a4ae9f
Andrei Grigoruta <androk20@gmail.com>
2023-02-06 14:21:27 +0200

    Resolve getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  // let titles = [];
+  // for (let book of books) {
+  //   titles.push(book.title);
+  // }
+  // return titles;
 
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
