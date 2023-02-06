ec20d728b1bee8195112a1cf5d973b451fe94ed9
Andrei Grigoruta <androk20@gmail.com>
2023-02-06 13:37:11 +0200

    Resolve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const wordWithoutSymb = word.toLowerCase().replace(/[\W\s]/g, "");
+  return wordWithoutSymb.split("").reverse().join("") === wordWithoutSymb;
 };
 
 // Do not edit below this line
